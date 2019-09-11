be9927e41143a72a0a546f0071f3a38ef82fe188
Boni Garcia <bgarcia@gsyc.es>
2019-09-11 16:10:37 +0200

    Include Selenium-Jupiter motivation tests

---
+++ src/test/java/io/github/bonigarcia/seljup/motivation/SeleniumJUnit4Test.java
@@ -0,0 +1,62 @@
+/*
+ * (C) Copyright 2019 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.seljup.motivation;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumJUnit4Test {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        System.setProperty("webdriver.chrome.driver",
+                "/home/boni/.m2/repository/webdriver/chromedriver/linux64/76.0.3809.126/chromedriver");
+
+        // FIXME: Path chromedriver in your system
+        // Alternatively, you can use WebDriverManager for this, see
+        // https://github.com/bonigarcia/webdrivermanager
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://bonigarcia.github.io/selenium-jupiter/");
+        assertThat(driver.getTitle(),
+                containsString("JUnit 5 extension for Selenium"));
+    }
+
+}
