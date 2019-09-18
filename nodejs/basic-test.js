812ad673d19c0e302028cdc59c0be2796afdf5c3
Boni Garcia <bgarcia@gsyc.es>
2019-09-18 18:01:38 +0200

    Include Node.js test against Selenium-Jupiter server

---
+++ nodejs/basic-test.js
@@ -0,0 +1,31 @@
+var webdriver = require("selenium-webdriver");
+
+async function basicTest() {
+	var seleniumServerUrl = "http://localhost:4042/wd/hub";
+	var capabilities = {
+		browserName : "chrome",
+		version: "77.0"
+	};
+
+	try {
+		var driver = await new webdriver.Builder().usingServer(seleniumServerUrl)
+				.withCapabilities(capabilities).build();
+
+		var sutUrl = "https://bonigarcia.github.io/selenium-jupiter/";
+		await driver.get(sutUrl);
+
+		await driver.getTitle().then(function(title) {
+			console.log("The title of " + sutUrl + " is '" + title + "'")
+		});
+
+	} catch (err) {
+		console.error("Something went wrong!\n", err.stack);
+
+	} finally {
+		if (driver) {
+			driver.quit();
+		}
+	}
+}
+
+basicTest();
